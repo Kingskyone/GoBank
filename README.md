71d22b07e0f324f0bfcc8f216975e27ae00c4093
wty102 <sdwangty@163.com>
2024-05-29 19:16:02 +0800

    更新部分

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
-使用 Golang、Postgres 实现的模拟银行操作逻辑
+使用 Golang、Postgres 实现的模拟银行操作逻辑,
+技术栈 Go、Gin、PostgresSQL、docker、git
 
+使用dbdiagram.io写SQL代码并将其转化为sql文件
+使用golang-migrate进行数据库版本管理
+使用sqlc进行ORM映射数据库操作函数的生成，使用pgx作为连接PostgreSQL的引擎
+对于sqlc生成的函数进行二次封装，并编写详细的测试操作
+基于gin编写服务器代码，包括路由对应的函数、路由组、验证器中间件等
+
+
+步骤:
 1. dbdiagram.io 写SQL代码 导出
 2. 使用`migratecreate`生成对应的migration up、down文件
 3. 使用`migrateup`生成数据库
--- test.go
+++ test.go
@@ -1,37 +1,16 @@
 package main
 
-//
 //func main() {
-//	fmt.Println(mostCompetitive([]int{3, 5, 2, 6}, 2))
+//	fmt.Println(findPeaks([]int{3, 5, 2, 6}))
 //	//fmt.Println(finMax([]int{5, 2, 1}, -1))
 //}
 //
-//func mostCompetitive(nums []int, k int) []int {
-//	res := make([]int, k)
-//	head := 0
-//	for ind, i := range nums {
-//		for true {
-//			if (len(nums) - ind) == (k - head) {
-//				res = append(res[:head], nums[ind:]...)
-//				return res
-//			}
-//			if head == 0 {
-//				res[head] = i
-//				head++
-//				break
-//			} else if head != 0 && i >= res[head-1] {
-//				if head < k {
-//					res[head] = i
-//					head++
-//					break
-//				} else {
-//					break
-//				}
-//			} else {
-//				head--
-//				continue
-//			}
+//func findPeaks(mountain []int) []int {
+//	var ret []int
+//	for i := 1; i < len(mountain)-1; i++ {
+//		if mountain[i] > mountain[i-1] && mountain[i] > mountain[i+1] {
+//			ret = append(ret, i)
 //		}
 //	}
-//	return res
+//	return ret
 //}
